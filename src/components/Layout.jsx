965de7ac2c53d1f96cd1c79835aca455032f32b6
Khristian Rampersad <ksrbot@hotmail.com>
2026-04-13 14:00:05 -0400

    Add ScrollToTop component to reset scroll position on route change

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,9 +1,22 @@
+import { useEffect } from 'react'
+import { useLocation } from 'react-router-dom'
 import Header from './Header'
 import Footer from './Footer'
+
+function ScrollToTop() {
+  const { pathname } = useLocation()
+
+  useEffect(() => {
+    window.scrollTo(0, 0)
+  }, [pathname])
+
+  return null
+}
 
 function Layout({ children }) {
   return (
     <div className="min-h-screen flex flex-col">
+      <ScrollToTop />
       <Header />
       <main className="flex-grow">
         {children}
